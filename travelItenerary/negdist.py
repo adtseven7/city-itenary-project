023c9bda3033e44c400a60b53837aef65ee14c01
Nilanjana Lodh <nilanjanalodh@gmail.com>
2018-08-26 15:42:12 +0530

    merge fix-missing-rating and master(from aditya)

---
+++ travelItenerary/negdist.py
@@ -0,0 +1,28 @@
+import os
+import django
+import math
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelItenerary.settings")
+
+django.setup()
+from iteneraryApplication.models import *
+
+DistTime = DistanceTime.objects.all()
+
+for disttimObject in DistTime:
+    distance = disttimObject.distance
+    if distance < 0:
+        [lat1, lon1] = [math.radians(disttimObject.source.latitude), math.radians(disttimObject.source.longitude)]
+        [lat2, lon2] = [math.radians(disttimObject.dest.latitude), math.radians(disttimObject.dest.longitude)]
+        R = 6373.0
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        distance = R * c * 1000
+        #print lat1, lon1, '>', lat2, lon2, '=', distance
+        disttimObject.distance = distance
+        disttimObject.save()
+
+
